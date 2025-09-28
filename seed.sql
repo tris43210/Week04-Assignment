b1a1da0dc68a41a33fc3a39cfa643ed43daa1d88
tris43210 <tristan.g.king@gmail.com>
2025-09-28 14:56:53 +0100

    re-styled track layout with grid

--- seed.sql
+++ seed.sql
@@ -8,3 +8,9 @@ CREATE TABLE musicStorage (
 
 ALTER TABLE musicstorage
 ADD COLUMN artistName TEXT;
+
+INSERT INTO musicStorage (trackName, BPM, "key", comments, artistName) 
+VALUES ('Fine Day Anthem', 138, '11A', 'Long Intro', 'Skrillex Boyz Noise & Opus III');
+
+INSERT INTO musicStorage (trackName, BPM, "key", comments, artistName) 
+VALUES ('Left to Right (Space Laces Remix)', 128, '2A', 'Left to Right Remix', 'Odd Mobb Space Laces'), ('Boost Up', 128, '8A', 'Groovy', 'Flowdan FISHER')
